512e1ca053e7ae599f21f1c1b020da4d2a202c89
Taraspetrenko04 <Taraspetrenko04@gmail.com>
2019-10-09 17:37:55 +0300

    new

--- test_project/src/js/script.js
+++ test_project/src/js/script.js
@@ -8,4 +8,26 @@ document.getElementsByClassName('navbar__burger')[0].addEventListener('click', f
 })
 
 
-}
+// Search EVENT Target
+// document.getElementsByClassName('navbar')[0].addEventListener('click', function(event) {
+//     let target = event.target;
+//     console.log(target.className);
+//     if (target.className === 'navbar__search') {
+//         document.getElementsByClassName('navbar__search')[0].classList.toggle('hide');
+//         document.getElementsByClassName('navbar__field')[0].classList.toggle('hide');
+//     } else if (target.className === 'navbar__field') {
+//         document.getElementsByClassName('navbar__search')[0].classList.toggle('hide');
+//         document.getElementsByClassName('navbar__field')[0].classList.toggle('hide');
+//     }
+// })
+document.getElementsByClassName('navbar__search')[0].addEventListener('click', function() {
+    document.getElementsByClassName('navbar__search')[0].classList.toggle('hide');
+    document.getElementsByClassName('navbar__field')[0].classList.toggle('hide');
+})
+
+document.getElementsByClassName('navbar__input')[0].addEventListener('blur', function() {
+    document.getElementsByClassName('navbar__search')[0].classList.toggle('hide');
+    document.getElementsByClassName('navbar__field')[0].classList.toggle('hide');
+})
+
+}//end
